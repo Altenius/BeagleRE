f9e5dea8c57abf8fe19fe0da1cc05135dd4343e9
Altenius <jacobjm18@gmail.com>
2018-11-12 20:36:13 -0500

    Started read_memory

--- src/memory.rs
+++ src/memory.rs
@@ -39,14 +39,36 @@ impl Section {
 
 impl Layout {
 	pub fn add_section(&mut self, offset: usize, section: Section) -> &mut Section {
+		// Insert the section in order
 		for (i, entry) in self.sections.iter().enumerate() {
 			if entry.0 > offset {
 				self.sections.insert(i, (offset, section));
 				return &mut self.sections.get_mut(i).unwrap().1;
 			}
 		}
-		// All entries were less than offset or none exist; append to the end
+		// All entries were less than offset or the array is empty; append to the end
 		self.sections.push((offset, section));
 		&mut self.sections.last_mut().unwrap().1
+	}
+
+	/// Reads memory at the address into the buffer. Returns the amount written to the buffer
+	pub fn read_memory(&self, start_address: usize, start_buffer: &mut [u8]) -> usize {
+		// Find the section containing the address
+		let start_section = self.sections.iter().enumerate().find(|(_, (sec_address, _section))| *sec_address <= start_address);
+		if start_section.is_none() {
+			return 0;
+		}
+		let (mut section_id, mut section) = start_section.unwrap();
+
+		let mut buffer = start_buffer;
+		let mut address = start_address;
+
+		while !buffer.is_empty() {
+			// Find the offset within the section
+			let offset = address - section.0;
+			
+		}
+
+		0
 	}
 }
